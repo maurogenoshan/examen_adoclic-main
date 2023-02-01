d2dd6808a38c656a9201dcd4d1e0f64a397fda41
Mauro Gaitan <mauro@genosha.com.ar>
2023-02-01 20:40:23 -0300

    Interface Config database configuration

---
+++ examen_adoclic-main/2_mysql_obj/ConfigDbInterface.php
@@ -0,0 +1,10 @@
+
+<?php
+interface ConfigDbInterface
+{
+    const DB_HOST = 'localhost';
+    const DB_USER = 'root';
+    const DB_PASSWORD = '';
+    const DB_NAME = 'adolic_examen';
+    const ATTR_ERRMODE = PDO::ERRMODE_EXCEPTION;
+}
