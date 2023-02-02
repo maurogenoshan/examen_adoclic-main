fe17e38727a95b645465ec73000cbf6fbbcba10f
Mauro Gaitan <mauro@genosha.com.ar>
2023-02-01 21:33:04 -0300

    Fetch All

--- examen_adoclic-main/2_mysql_obj/UserStats.php
+++ examen_adoclic-main/2_mysql_obj/UserStats.php
@@ -7,9 +7,9 @@ class UserStats extends DbConnection
     public function __construct(string $dateFrom, string $dateTo, int $totalClicks = null)
     {
 
-        $this->dateFrom = $dateFrom;
-        $this->dateTo = $dateTo;
-        $this->totalClicks = $totalClicks;
+        $this->dateFrom =  $this->get_string_to_date($dateFrom);
+        $this->dateTo =  $this->get_string_to_date($dateTo);
+        $this->totalClicks = (int) $totalClicks;
 
         $this->connection =  (new DbConnection())->get_connection();
     }
@@ -23,14 +23,14 @@ class UserStats extends DbConnection
     {
         $query = $this->connection->prepare("SELECT users.id,DATE(user_stats.date) as date_stats, user_stats.views,user_stats.clicks,user_stats.conversions, users.status FROM user_stats INNER JOIN users ON user_stats.user_id = users.id WHERE DATE(user_stats.date) > :dateFrom AND DATE(user_stats.date) < :dateTo AND users.status = 'active' AND user_stats.clicks > :totalClicks ORDER BY date ASC");
 
-        $query->bindParam(':dateFrom', $this->get_string_to_date($this->dateFrom), PDO::PARAM_STR, 10);
-        $query->bindParam(':dateTo', $this->get_string_to_date($this->dateTo), PDO::PARAM_STR, 10);
-        $query->bindParam(':totalClicks', $this->totalClicks, PDO::PARAM_INT, 10);
+
+        $query->bindParam(':dateFrom', $this->dateFrom, PDO::PARAM_STR);
+        $query->bindParam(':dateTo', $this->dateTo, PDO::PARAM_STR);
+        $query->bindParam(':totalClicks', $this->totalClicks, PDO::PARAM_INT);
 
         $query->execute();
 
-        $user_stats = $query->fetch(PDO::FETCH_ASSOC);
-
+        $user_stats = $query->fetchaLL(PDO::FETCH_ASSOC);
         if (!$user_stats)  return [];
         return $user_stats;
     }
