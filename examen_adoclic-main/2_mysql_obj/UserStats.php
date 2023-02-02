91b519a80a7540461a59beff1e52ca1b85bb30f1
Mauro Gaitan <mauro@genosha.com.ar>
2023-02-01 22:37:26 -0300

    Conditionally params

--- examen_adoclic-main/2_mysql_obj/UserStats.php
+++ examen_adoclic-main/2_mysql_obj/UserStats.php
@@ -19,14 +19,38 @@ class UserStats extends DbConnection
         return date('Y-m-d', strtotime($string));
     }
 
+    private function is_range_of_dates()
+    {
+        return isset($this->dateFrom) && !empty($this->dateFrom) && isset($this->dateTo) && !empty($this->dateTo);
+    }
+
+    private function is_clicks_not_empty()
+    {
+        return isset($this->totalClicks) && !empty($this->totalClicks);
+    }
+    private function get_where_conditions_conditionally()
+    {
+        $where = " users.status = 'active'";
+        $where .= ($this->is_range_of_dates()) ? " AND DATE(user_stats.date) > :dateFrom AND DATE(user_stats.date) < :dateTo" : '';
+        $where .= ($this->is_clicks_not_empty()) ? " AND user_stats.clicks > :totalClicks" : '';
+
+        return $where;
+    }
+
     public function get_stats()
     {
-        $query = $this->connection->prepare("SELECT users.id,DATE(user_stats.date) as date_stats, user_stats.views,user_stats.clicks,user_stats.conversions, users.status FROM user_stats INNER JOIN users ON user_stats.user_id = users.id WHERE DATE(user_stats.date) > :dateFrom AND DATE(user_stats.date) < :dateTo AND users.status = 'active' AND user_stats.clicks > :totalClicks ORDER BY date ASC");
+        $where = $this->get_where_conditions_conditionally();
 
 
-        $query->bindParam(':dateFrom', $this->dateFrom, PDO::PARAM_STR);
-        $query->bindParam(':dateTo', $this->dateTo, PDO::PARAM_STR);
-        $query->bindParam(':totalClicks', $this->totalClicks, PDO::PARAM_INT);
+        $query = $this->connection->prepare("SELECT users.id,DATE(user_stats.date) as date_stats, user_stats.views,user_stats.clicks,user_stats.conversions, users.status FROM user_stats INNER JOIN users ON user_stats.user_id = users.id WHERE $where ORDER BY date ASC");
+        if ($this->is_range_of_dates()) {
+            $query->bindParam(':dateFrom', $this->dateFrom, PDO::PARAM_STR);
+            $query->bindParam(':dateTo', $this->dateTo, PDO::PARAM_STR);
+        }
+        if ($this->is_clicks_not_empty()) {
+            $query->bindParam(':totalClicks', $this->totalClicks, PDO::PARAM_INT);
+        }
+
 
         $query->execute();
 
