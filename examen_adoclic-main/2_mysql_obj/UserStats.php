a521079238bca22529171abf68279a4035366aa4
Mauro Gaitan <mauro@genosha.com.ar>
2023-02-01 21:12:14 -0300

    Bind params query

--- examen_adoclic-main/2_mysql_obj/UserStats.php
+++ examen_adoclic-main/2_mysql_obj/UserStats.php
@@ -14,24 +14,24 @@ class UserStats extends DbConnection
         $this->connection =  (new DbConnection())->get_connection();
     }
 
+    private function get_string_to_date($string)
+    {
+        return date('Y-m-d', strtotime($string));
+    }
+
     public function get_stats()
     {
-        $statement = $this->connection->prepare("SELECT * FROM user_stats WHERE DATE('date','Y-M-D') BETWEEN :dateFrom AND :dateTo");
+        $query = $this->connection->prepare("SELECT users.id,DATE(user_stats.date) as date_stats, user_stats.views,user_stats.clicks,user_stats.conversions, users.status FROM user_stats INNER JOIN users ON user_stats.user_id = users.id WHERE DATE(user_stats.date) > :dateFrom AND DATE(user_stats.date) < :dateTo AND users.status = 'active' AND user_stats.clicks > :totalClicks ORDER BY date ASC");
 
-        $statement->execute(
-            array(
-                ":dateFrom" => date("Y-M-D", strtotime($this->dateFrom)), PDO::PARAM_STR,
-                ":dateTo" => date("Y-M-D", strtotime($this->dateFrom)), PDO::PARAM_STR
-            )
-        );
+        $query->bindParam(':dateFrom', $this->get_string_to_date($this->dateFrom), PDO::PARAM_STR, 10);
+        $query->bindParam(':dateTo', $this->get_string_to_date($this->dateTo), PDO::PARAM_STR, 10);
+        $query->bindParam(':totalClicks', $this->totalClicks, PDO::PARAM_INT, 10);
 
+        $query->execute();
 
-        $publisher = $statement->fetch(PDO::FETCH_ASSOC);
-        var_dump($publisher);
-        if ($publisher) {
-            echo $publisher['userId'] . '.' . $publisher['views'];
-        } else {
-            echo "The publisher with id $this->dateTo was not found.";
-        }
+        $user_stats = $query->fetch(PDO::FETCH_ASSOC);
+
+        if (!$user_stats)  return [];
+        return $user_stats;
     }
 }
